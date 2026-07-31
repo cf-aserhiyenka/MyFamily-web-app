484c1b5fafdeed90d73bf0d78358cad315bb7e65
cf-aserhiyenka <104493566+cf-aserhiyenka@users.noreply.github.com>
2026-07-31 10:37:40 +0200

    fix warning (#28)

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -4,4 +4,4 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 export const prisma = new PrismaClient({ adapter });
 
-export * from "@prisma/client";
+export { MemberStatus, FamilyRole, AlbumType, MediaType } from "@prisma/client";
